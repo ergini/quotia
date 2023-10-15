d551b016f7fd3e86e669dc454f9d8e5cf10b44ee
Ergo <ergin@tetbit.com>
2023-10-15 14:57:08 +0200

    Description Update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,7 +17,7 @@ export function absoluteUrl(path: string) {
 
 export function constructMetadata({
   title = "Quotia - Let's chat to your PDF files",
-  description = "Quill is a software to make chatting to your PDF files easy.",
+  description = "Quotia is a software to make chatting to your PDF files easy.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
   noIndex = false
